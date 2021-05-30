da1d236a70ab2d949e0b15f860e1a861e31da8bb
Riski Midi Wardana <riskimidiw@gmail.com>
2021-05-30 07:18:20 +0700

    add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ RUN apt-get update && apt-get upgrade -y
 
 RUN docker-php-ext-install mysqli
 EXPOSE 80
+RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
 RUN a2enmod rewrite
 RUN chmod -R 777 /var/www/html/
--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'db',
-	'username' => 'test',
-	'password' => 'test',
-	'database' => 'test_docker',
+	'hostname' => getenv("DB_HOST"),
+	'username' => getenv("DB_USERNAME"),
+	'password' => getenv("DB_PASSWORD"),
+	'database' => getenv("DB_NAME"),
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
